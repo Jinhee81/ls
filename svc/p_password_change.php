57bbaa14632e923edc568bf1a4118bf31ced2dc8
charm19 <smswms1112@gmail.com>
2021-04-05 16:17:48 +0900

    비밀번호 변경 부문 마무리

--- svc/p_password_change.php
+++ svc/p_password_change.php
@@ -1,18 +1,21 @@
 <?php
 session_start();
+header('Content-Type: text/html; charset=UTF-8');
 include $_SERVER['DOCUMENT_ROOT']."/svc/view/conn.php";
-include "password.php";
 
-// $password = $_POST['password'];
-// $hash = password_hash($password, PASSWORD_DEFAULT);
+// print_r($_SESSION);
+// ini_set('display_errors', 1);
+// ini_set('error_reporting', E_ALL);
+// // $password = $_POST['password'];
+// // $hash = password_hash($password, PASSWORD_DEFAULT);
 
-// print_r($_POST);
+// // print_r($_POST);
 
 $sql = "select password from user where id={$_SESSION['id']}";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_array($result);
 
-if(password_verify($_POST['password2'], $row[0])){
+if($_POST['password2'] == $row[0]){
   echo "<script>
   alert('기존 비밀번호와 동일해서 변경 불가합니다.');
   location.href='password_change.php';
